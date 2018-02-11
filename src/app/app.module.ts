ab8d012cf0ddcf13980ce4f80872a1f2934e5640
Timofei Iatsenko <timofei_iatsenko@epam.com>
2018-02-11 15:49:45 +0100

    remove unused directives

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,23 +1,21 @@
 import { HttpClientModule } from '@angular/common/http';
-import {BrowserModule} from '@angular/platform-browser';
-import {NgModule} from '@angular/core';
-import { FloorPlanComponent } from './components/floor-plan/floor-plan.component';
-import { ButtonComponent } from './components/icon-button/icon-button.component';
-
-import {HomeComponent} from './home/home.component';
-import {ZoneControlComponent} from './controls/zone-control/zone-control.component';
-import {TempControlComponent} from './controls/temp-control/temp-control.component';
-import {ReconnectOverlayComponent} from './controls/reconnect-overlay/reconnect-overlay.component';
-import {ZoneDirective} from './directives/zone.directive';
-import {SettingsPanelComponent} from './settings/settings-panel/settings-panel.component';
-import {StatusAreaComponent} from './controls/status-area/status-area.component';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
 import { InlineSVGModule } from 'ng-inline-svg';
 
 // App is our top level component
-import {AppComponent} from './app.component';
+import { AppComponent } from './app.component';
+import { COMMON_DIRECTIVES } from './common/directives';
+import { FloorPlanComponent } from './components/floor-plan/floor-plan.component';
+import { ButtonComponent } from './components/icon-button/icon-button.component';
+import { ReconnectOverlayComponent } from './controls/reconnect-overlay/reconnect-overlay.component';
+import { StatusAreaComponent } from './controls/status-area/status-area.component';
+import { TempControlComponent } from './controls/temp-control/temp-control.component';
+import { ZoneControlComponent } from './controls/zone-control/zone-control.component';
+import { ZoneDirective } from './directives/zone.directive';
 
-import '../styles.scss';
-import {COMMON_DIRECTIVES} from './common/directives';
+import { HomeComponent } from './home/home.component';
+import { SettingsPanelComponent } from './settings/settings-panel/settings-panel.component';
 
 
 /**
--- src/app/common/directives/index.ts
+++ src/app/common/directives/index.ts
@@ -1,9 +1,5 @@
 import { WavesDirective } from './waves.directive';
-import { MDirective } from './m.directive';
-import { XLinkDirective } from './xlink.directive';
 
 export const COMMON_DIRECTIVES = [
   WavesDirective,
-  MDirective,
-  XLinkDirective
 ];
